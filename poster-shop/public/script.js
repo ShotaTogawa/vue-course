7fe6b565b9706b52cae0899e3d289f51294dedee
ShotaTogawa <s.togawa0112@gmail.com>
2020-04-24 14:16:24 +0900

    basic v-bind v-for v-if v-on:click

--- poster-shop/public/script.js
+++ poster-shop/public/script.js
@@ -1,1 +1,17 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{ id: 1, title: "product1" },
+			{ id: 2, title: "product2" },
+			{ id: 3, title: "product3" },
+		],
+		cart: [],
+	},
+	methods: {
+		addToCart: function (product) {
+			console.log(product.id);
+		},
+	},
+});
