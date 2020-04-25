5bdca115095717a500b71daf352177fbac3dd186
ShotaTogawa <s.togawa0112@gmail.com>
2020-04-25 12:44:16 +0900

    finished section 12

--- vuejs-cinema/src/main.js
+++ vuejs-cinema/src/main.js
@@ -1,9 +1,45 @@
 import './style.scss';
 import Vue from 'vue';
+import genres from './util/genres';
 
 new Vue({
   el: '#app',
-  data: {
-    msg: 'Hello world',
+  components: {
+    'movie-list': {
+      template: `
+            <div id="movie-list">
+                <div v-for="movie in movies" class="movie">
+                    {{ movie.title }}
+                </div>
+            </div>`,
+      data() {
+        return {
+          movies: [
+            { title: 'Pulp fiction' },
+            { title: 'Hoem Alone' },
+            { title: 'Harry Potter' },
+          ],
+        };
+      },
+    },
+    'movie-filter': {
+      data() {
+        return {
+          genres,
+        };
+      },
+      template: `
+            <div id="movie-filter">
+              <h2>Filter Results</h2>
+              <div class="filter-group">
+                <check-filter v-for="genre in genres"></check-filter>
+              </div>
+            </>`,
+      components: {
+        'check-filter': {
+          template: `<div>Filter</div>`,
+        },
+      },
+    },
   },
 });
