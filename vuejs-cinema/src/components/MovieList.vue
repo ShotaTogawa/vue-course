be5385b448a376696c8c19047886642130921e13
ShotaTogawa <s.togawa0112@gmail.com>
2020-04-26 09:10:50 +0900

    refactored to single components

---
+++ vuejs-cinema/src/components/MovieList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="movie-list">
+    <div v-for="movie in filteredMovies" class="movie">
+      {{ movie.title }}
+    </div>
+  </div>
+  `
+</template>
+
+<script>
+import genres from '../util/genres';
+
+export default {
+  data() {
+    return {
+      movies: [
+        { title: 'Pulp fiction', genre: genres.CRIME },
+        { title: 'Home Alone', genre: genres.COMEDY },
+        { title: 'Harry Potter', genre: genres.FANTASY },
+      ],
+    };
+  },
+  props: ['genre', 'time'],
+  methods: {
+    moviePassesGenreFilter(movie) {
+      if (!this.genre.length) {
+        return true;
+      } else {
+        return this.genre.find((genre) => movie.genre === genre);
+      }
+    },
+  },
+  computed: {
+    filteredMovies() {
+      return this.movies.filter(this.moviePassesGenreFilter);
+    },
+  },
+};
+</script>
