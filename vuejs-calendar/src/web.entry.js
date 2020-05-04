bdf0c64ba7172a2018a3e18dba0941c5aba15e6c
ShotaTogawa <s.togawa0112@gmail.com>
2020-05-04 12:07:42 +0900

    fnished sec 29

--- vuejs-calendar/src/web.entry.js
+++ vuejs-calendar/src/web.entry.js
@@ -7,6 +7,19 @@ import App from './components/App.vue';
 import store from './store';
 
 import moment from 'moment-timezone';
+
+let events = window.__INITIAL_STATE__.map((event) => {
+  return {
+    description: event.description,
+    date: moment(event.date),
+  };
+});
+
+let initialState = Object.assign({}, store.state, {
+  events,
+});
+store.replaceState(initialState);
+
 moment.tz.setDefault('UTC');
 Object.defineProperty(Vue.prototype, '$moment', {
   get() {
