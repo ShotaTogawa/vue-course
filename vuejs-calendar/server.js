6e59f6e19a95ffb99c9f1a7e1d1524096f274fce
ShotaTogawa <s.togawa0112@gmail.com>
2020-05-05 23:25:07 +0900

    started ssr section

--- vuejs-calendar/server.js
+++ vuejs-calendar/server.js
@@ -11,30 +11,52 @@ const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-let events = [];
+let events = [
+  { description: 'Random event 1', date: moment('2017-02-06', 'YYYY-MM-DD') },
+  { description: 'Random event 2', date: moment('2017-02-15', 'YYYY-MM-DD') },
+  { description: 'Random event 3', date: moment('2017-03-14', 'YYYY-MM-DD') },
+];
+
+let renderer;
 
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
   let contentMarker = '<!--APP-->';
-  res.send(
-    template.replace(
-      contentMarker,
-      `<script>var __INITIAL_STATE__ = ${serialize(events)}</script>`
-    )
-  );
+  if (renderer) {
+    renderer.renderToString({}, (err, html) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(
+          template.replace(
+            contentMarker,
+            `<script>var __INITIAL_STATE__ = ${serialize(
+              events
+            )}</script>\n${html}`
+          )
+        );
+      }
+    });
+  } else {
+    res.send('<p>Awaiting compilation..</p>');
+  }
 });
 
 app.use(require('body-parser').json());
 app.post('/add_event', (req, res) => {
   events.push(req.body);
+  res.sendStatus(200);
 });
 
 const server = http.createServer(app);
 
 if (process.env.NODE_ENV === 'development') {
   const reload = require('reload');
-  const reloadServer = reload(app);
+  const reloadServer = reload(server, app);
   require('./webpack-dev-middleware').init(app);
+  require('./webpack-server-compiler').init(function (bundle) {
+    renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+  });
 }
 
 server.listen(process.env.PORT, function () {
